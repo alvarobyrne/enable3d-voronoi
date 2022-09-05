3889a5157f199ff157577582d35886e52dd10292
Alvaro Obyrne <alvarobyrne@gmail.com>
2022-09-04 23:30:16 -0500

    remove enable3d standalone version from forked template

--- www/scripts/index.ts
+++ www/scripts/index.ts
@@ -1,9 +1,1 @@
-/**
- * Uncomment the scene you want to run. (default three.js)
- */
-
-// start three.js scene (with enable3d physics)
 import "./three";
-
-// start standalone enable3d scene
-// import "./standalone";
